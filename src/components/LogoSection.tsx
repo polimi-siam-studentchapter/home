cca65b59b11a8ad4596faa665a47a447cf6c5699
Giacomo Lorenzon <giacomo.lorenzon@mail.polimi.it>
2025-11-14 16:20:30 +0100

    search

--- src/components/LogoSection.tsx
+++ src/components/LogoSection.tsx
@@ -1,7 +1,7 @@
 // src/components/LogoSection.tsx
 import React from "react";
 import { LogoLoop } from "../components/LogoLoop";
-import '../assets/styles/Main.scss'; // make sure the styles are imported
+import "../assets/styles/Main.scss";
 
 interface LogoSectionProps {
   mode: "dark" | "light";
@@ -12,17 +12,46 @@ const LogoSection: React.FC<LogoSectionProps> = ({ mode }) => {
     <div className="logo-section">
       <h2>Institutions involved</h2>
       <p>Sponsors and institutions involved.</p>
+
       <section>
         <LogoLoop
           className="logo-loop"
           logos={[
-            { src: "./logos/logo_mathLab_bg_v.png", alt: "mathlab" },
-            { src: "./logos/logo_polimi.png", alt: "Polimi" },
-            { src: "./logos/logo-8192px.png", alt: "AIM" },
-            { src: "./logos/matematica_unipi_dx_blu.png", alt: "UniPi" },
-            { src: "./logos/SNS.png", alt: "SNS" },
-            { src: "./logos/pavia.png", alt: "Pavia" },
-            { src: "./logos/siamgenova.png", alt: "Genova" },
+            {
+              src: "./logos/logo_mathLab_bg_v.png",
+              alt: "mathLab",
+              href: "https://mathlab.sissa.it/"
+            },
+            {
+              src: "./logos/logo_polimi.png",
+              alt: "Politecnico di Milano",
+              href: "https://www.polimi.it/"
+            },
+            {
+              src: "./logos/logo-8192px.png",
+              alt: "AIM",
+              href: "https://www.aim-mate.it/"
+            },
+            {
+              src: "./logos/matematica_unipi_dx_blu.png",
+              alt: "Università di Pisa",
+              href: "https://www.unipi.it/"
+            },
+            {
+              src: "./logos/SNS.png",
+              alt: "Scuola Normale Superiore",
+              href: "https://www.sns.it/"
+            },
+            {
+              src: "./logos/pavia.png",
+              alt: "Università di Pavia",
+              href: "https://www.unipv.it/"
+            },
+            {
+              src: "./logos/siamgenova.png",
+              alt: "SIAM Genova",
+              href: "https://www.dima.unige.it/"
+            }
           ]}
           speed={80}
           direction="left"
